e92a724c6ed1f62bd1cfc2414cb6c4382c4de0d3
Alan1327 <palaciosma.pf@gmail.com>
2018-06-13 22:23:41 -0500

    Practica de commit

---
+++ suma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v1, v2, r;
+    cout << "Ingresar primer digito"<< endl;
+    cin>>v1;
+    cout << "Ingresar primer digito"<< endl;
+    cin>>v2;
+    r=v1+v2;
+    cout<<"Resultado es:"<<r;
+
+
+
+    return 0;
+}
